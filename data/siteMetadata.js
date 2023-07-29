f689ef670a94453047d9173edb1db8b99cbe55e5
mikumuu <mikimuuu@gmail.com>
2023-07-29 23:02:43 +0900

    utter

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -66,8 +66,8 @@ const siteMetadata = {
     utterancesConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://utteranc.es/
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
-      issueTerm: '', // supported options: pathname, url, title
+      repo: mikimuu/cds,
+      issueTerm: 'url', // supported options: pathname, url, title
       label: '', // label (optional): Comment 💬
       // theme example: github-light, github-dark, preferred-color-scheme
       // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
