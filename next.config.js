db67f7f5c009526a975d6cf24275596e8cd39dd4
mikumuu <mikimuuu@gmail.com>
2023-08-01 00:44:53 +0900

    a

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const ContentSecurityPolicy = 
 `
 default-src 'self';
-script-src 'self' platform.twitter.com syndication.twitter.com; giscus.app;
+script-src 'self' platform.twitter.com syndication.twitter.com; giscus.app; vitals.vercel-insights.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
