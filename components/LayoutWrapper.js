980af22c674f8a6a5f95814bb7ca5ca6ed0cc12c
mikumuu <mikimuuu@gmail.com>
2023-11-30 17:11:08 +0900

    comon

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -9,12 +9,12 @@ import ThemeSwitch from './ThemeSwitch'
 
 const LayoutWrapper = ({ children }) => {
   return (
-    <SectionContainer className="bg-brsky-blue text-brblue">
-      <header className="bg-brsoftblue p-5 border-b-4 border-brgreen">
-        <h1 className="text-4xl font-bold text-brorange">
+    <SectionContainer className="bg-brblue text-gray-100">
+      <header className="p-5 border-b-4 border-brgreen">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">
           {siteMetadata.title}
         </h1>
-        <div className="my-5">
+        <div className="my-3 md:my-5">
           <Logo />
         </div>
         <nav className="flex justify-between">
@@ -22,26 +22,25 @@ const LayoutWrapper = ({ children }) => {
             <Link
               key={link.title}
               href={link.href}
-              className="text-2xl font-bold text-bryellow px-2"
+              className="text-lg md:text-xl lg:text-2xl font-medium px-2"
             >
               {link.title}
             </Link>
           ))}
         </nav>
-        <div className="text-3xl font-bold text-brviolet rotate-12">
+        <div className="text-2xl md:text-3xl font-medium rotate-12">
           日記を書いています
         </div>
-        <div className="mt-5">
+        <div className="mt-3 md:mt-5">
           <ThemeSwitch />
         </div>
       </header>
       <main className="p-5">
         {children}
       </main>
-      <Footer className="bg-brcyan p-5 border-t-4 border-brred" />
+      <Footer className="p-5 border-t-4 border-brred" />
     </SectionContainer>
   )
 }
 
 export default LayoutWrapper
-
