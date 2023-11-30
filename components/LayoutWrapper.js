239fbd6188ecc214e1148b9665b8ac8a18359e6b
mikumuu <mikimuuu@gmail.com>
2023-11-30 20:05:17 +0900

    どうすかねぇ

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/20.svg'
+import Logo from '@/data/20.jpg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
