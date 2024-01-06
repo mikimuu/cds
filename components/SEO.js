1701dafa42e75faa5898792d8f91423440995f28
mikumuu <mikimuuu@gmail.com>
2024-01-07 03:38:20 +0900

    gogo

--- components/SEO.js
+++ components/SEO.js
@@ -29,6 +29,8 @@ const CommonSEO = ({ title, description, ogType, ogImage, twImage, canonicalUrl 
         rel="canonical"
         href={canonicalUrl ? canonicalUrl : `${siteMetadata.siteUrl}${router.asPath}`}
       />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7341612625193776"
+     crossorigin="anonymous"></script>
     </Head>
   )
 }
